4136a7965fed132adb6817df933f6ce8dd5f03cc
Karthikeyan Janakiraman <janakiraman.karthikeyan@gmail.com>
2016-11-18 00:49:41 +0800

    This is my first commit. Commiting all the files into git.

---
+++ for-loop.js
@@ -0,0 +1,7 @@
+var total = 0;
+var limit = 10;
+var i=0;
+for (total; total < limit; total++){
+  i += total;
+}
+console.log(i);
---
+++ variables.js
@@ -0,0 +1,4 @@
+var example;
+var example1='some string';
+
+console.log(example1);
